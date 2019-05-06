c77ed0d513fad42353db41ef5f9b92256ccb41ca
Andi <andi.basic10@gmail.com>
2019-05-06 15:29:10 +0200

    api fetch&props with component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './App.css';
+import Form from "./components/Form"
+import Recipes from "./components/Recipes"
+
+const API = "6c5fb4e986d45bc7e606e42f70dc33bc"
+
+
+
+class App extends Component{
+  state = {
+    recipes : []
+  }
+  getRecipe = async (e) => { 
+    const recipeName = e.target.elements.recipeName.value
+    e.preventDefault()
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API}&q=${recipeName}&count=10`)
+    const data = await api_call.json()
+    this.setState({ recipes : data.recipes })
+    console.log(this.state.recipes)
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Recipe search </h1>
+        </header>
+        <Form getRecipe = {this.getRecipe}  />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
